cb5745d4934e8dcd76183663431455701f891b6a
Rodrigo <falcaorw@gmail.com>
2018-04-19 16:56:46 -0300

    inicio da implementação da requisição da API

--- app/src/main/java/com/example/android/pi2/MedicationSearch.java
+++ app/src/main/java/com/example/android/pi2/MedicationSearch.java
@@ -2,12 +2,28 @@ package com.example.android.pi2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MedicationSearch extends AppCompatActivity {
+
+    Button medSearchBtn;
+    TextView jsonContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medication_search);
+
+        medSearchBtn = findViewById(R.id.medJsonBtn);
+        jsonContent = findViewById(R.id.jsonText);
+
+        medSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/otherClasses/fetchJson.java
@@ -0,0 +1,28 @@
+package otherClasses;
+
+import android.os.AsyncTask;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by 1712130027 on 19/04/2018.
+ */
+
+public class fetchJson extends AsyncTask<Void,Void,Void> {
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL jsonUrl = new URL("http://mobile-aceite.tcu.gov.br/mapa-da-saude/rest/remedios");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+}
