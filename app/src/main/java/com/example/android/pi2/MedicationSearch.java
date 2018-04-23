f3893ad0589a8681a321b5f12e8830a9c0b87798
Rodrigo <falcaorw@gmail.com>
2018-04-23 17:02:59 -0300

    recycle view com chamada de api e filtro por string de nome implementado

--- app/src/main/java/com/example/android/pi2/MedicationSearch.java
+++ app/src/main/java/com/example/android/pi2/MedicationSearch.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,7 +18,9 @@ import com.example.android.util.Api;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -26,6 +30,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MedicationSearch extends AppCompatActivity {
 
     Button medSearchBtn;
+    Button filterBtn;
+    EditText filterText;
     TextView jsonContent;
     RecyclerView medRecView;
     MedicationAdapter medAdapter;
@@ -37,13 +43,16 @@ public class MedicationSearch extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medication_search);
 
+        filterBtn = findViewById(R.id.filterBtn);
+        filterText = findViewById(R.id.filterText);
+
         medList = new ArrayList<>();
 
         medRecView = findViewById(R.id.medRecycleView);
         medRecView.setHasFixedSize(true);
         medRecView.setLayoutManager(new LinearLayoutManager(this));
 
-        medList.add(new Medication("VIROTIN", "ACICLOVIR", "ASPEN PHARMA INDÚSTRIA FARMACÊUTICA LTDA", "400 MG COM CT BL AL PLAS INC X 70", "L02A3 - ANÁLOGOS HORMONA…DOTROFINAS CITOSTÁTICOS", "Não", 331.30));
+        /*medList.add(new Medication("VIROTIN", "ACICLOVIR", "ASPEN PHARMA INDÚSTRIA FARMACÊUTICA LTDA", "400 MG COM CT BL AL PLAS INC X 70", "L02A3 - ANÁLOGOS HORMONA…DOTROFINAS CITOSTÁTICOS", "Não", 331.30));
         medList.add(new Medication("CANCIDAS", "ACETATO DE CASPOFUNGINA", "ERCK SHARP & DOHME FARMACEUTICA  LTDA", "70 MG PO LIOF SOL INJ CT FA VD INC", "L02A3 - ANÁLOGOS HORMONA…DOTROFINAS CITOSTÁTICOS", "Não", 4247.26));
         medList.add(new Medication("NOLUPRON DEPOTME", "ACETATO DE LEUPRORRELINA", "ABBVIE FARMACÊUTICA LTDA", "5,0 MG/ML SOL INJ PT PLA…SER + 15 SACHETS ALCOOL", "L02A3 - ANÁLOGOS HORMONA…DOTROFINAS CITOSTÁTICOS", "Não", 725.23));
         medList.add(new Medication("VIROTIN", "ACICLOVIR", "ASPEN PHARMA INDÚSTRIA FARMACÊUTICA LTDA", "400 MG COM CT BL AL PLAS INC X 70", "L02A3 - ANÁLOGOS HORMONA…DOTROFINAS CITOSTÁTICOS", "Não", 331.30));
@@ -55,8 +64,14 @@ public class MedicationSearch extends AppCompatActivity {
         medList.add(new Medication("VIROTIN", "ACICLOVIR", "ASPEN PHARMA INDÚSTRIA FARMACÊUTICA LTDA", "400 MG COM CT BL AL PLAS INC X 70", "L02A3 - ANÁLOGOS HORMONA…DOTROFINAS CITOSTÁTICOS", "Não", 331.30));
         medList.add(new Medication("CANCIDAS", "ACETATO DE CASPOFUNGINA", "ERCK SHARP & DOHME FARMACEUTICA  LTDA", "70 MG PO LIOF SOL INJ CT FA VD INC", "L02A3 - ANÁLOGOS HORMONA…DOTROFINAS CITOSTÁTICOS", "Não", 4247.26));
         medList.add(new Medication("NOLUPRON DEPOTME", "ACETATO DE LEUPRORRELINA", "ABBVIE FARMACÊUTICA LTDA", "5,0 MG/ML SOL INJ PT PLA…SER + 15 SACHETS ALCOOL", "L02A3 - ANÁLOGOS HORMONA…DOTROFINAS CITOSTÁTICOS", "Não", 725.23));
+*/
 
-        Retrofit retrofit = new Retrofit.Builder().baseUrl(Api.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(1, TimeUnit.MINUTES)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(15, TimeUnit.SECONDS)
+                .build();
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(Api.BASE_URL).client(okHttpClient).addConverterFactory(GsonConverterFactory.create()).build();
 
         Api api = retrofit.create(Api.class);
 
@@ -67,8 +82,36 @@ public class MedicationSearch extends AppCompatActivity {
             public void onResponse(Call<List<Medication>> call, Response<List<Medication>> response) {
                 List<Medication> medications = response.body();
                 for(Medication med: medications){
-                    Log.d("nome", med.getNome());
+                    double medPreco = med.getPreco();
+                    medList.add(new Medication(med.getNome(), med.getpAtivo(), med.getLab(), med.getDesc(), med.gettClass(), med.getResctric(), med.getPreco()));
                 }
+                medAdapter = new MedicationAdapter(MedicationSearch.this, medList);
+                medRecView.setAdapter(medAdapter);
+
+                /*PENSAR NISSO*/
+                filterText.setOnKeyListener(new View.OnKeyListener() {
+                    @Override
+                    public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                        String query = filterText.getText().toString();
+
+                        List<Medication> tempList = new ArrayList<>();
+
+                        for(Medication tempMed:medList){
+                            if(tempMed.getNome().toLowerCase().contains(query.toLowerCase())){
+                                tempList.add(tempMed);
+                            }else if(query.matches("")){
+                                tempList = medList;
+                            }
+                        }
+
+                        medAdapter = new MedicationAdapter(MedicationSearch.this, tempList);
+                        medRecView.setAdapter(medAdapter);
+
+
+                        return false;
+                    }
+                });
+                /*PENSAR NISSO*/
             }
 
             @Override
@@ -81,8 +124,7 @@ public class MedicationSearch extends AppCompatActivity {
 
 
 
-        medAdapter = new MedicationAdapter(this, medList);
-        medRecView.setAdapter(medAdapter);
+
 
 
         //medSearchBtn = findViewById(R.id.medJsonBtn);
@@ -94,5 +136,29 @@ public class MedicationSearch extends AppCompatActivity {
 
             }
         });*/
+
+
+        /*PENSAR NISSO*/
+        filterText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                String query = filterText.getText().toString();
+
+                List<Medication> tempList = new ArrayList<>();
+
+
+                return false;
+            }
+        });
+        /*PENSAR NISSO*/
+
+        filterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                medList.remove(medList.size() - 1);
+                medAdapter = new MedicationAdapter(MedicationSearch.this, medList);
+                medRecView.setAdapter(medAdapter);
+            }
+        });
     }
 }
